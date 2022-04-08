79af5391c5d5087d5827a182d0733c5a71f6f8bc
Kawsar A <itskawsar@gmail.com>
2022-04-09 02:39:08 +0600

    added more logic to Localization middleware

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -16,8 +16,27 @@ class Localization
      */
     public function handle(Request $request, Closure $next)
     {
-        if (request()->segment(1) && in_array(request()->segment(1), config('app.available_locales'))) {
-            app()->setLocale(request()->segment(1));
+        $current_path = $request->path();
+        if ($request->segment(1) && in_array($request->segment(1), config('app.available_locales'))) {
+            session()->put('locale', $request->segment(1));
+            app()->setLocale(session()->get('locale'));
+        }
+        if ($request->segment(1) && strlen($request->segment(1)) != 2) {
+            if (session()->has('locale')) {
+                app()->setLocale(session()->get('locale'));
+            }
+        }
+        if (app()->getLocale() == null) {
+            // app()->setLocale(config('app.locale'));
+            if (session()->has('locale')) {
+                app()->setLocale(session()->get('locale'));
+            }
+        }
+        if ($request->segment(1) == null) {
+            return redirect('/'.app()->getLocale());
+        }
+        if ($request->segment(1) && strlen($request->segment(1)) != 2) {
+            return redirect('/'.app()->getLocale().'/'.$current_path);
         }
         return $next($request);
     }
