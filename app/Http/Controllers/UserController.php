d55c526d492e64641d7f7518cd95d8834e6c2fa0
Kawsar A <itskawsar@gmail.com>
2022-04-15 20:47:34 +0600

    added user index logic and template

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -13,7 +14,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::orderBy('id', 'DESC')->paginate(5);
+        return view('users.index', compact('users'));
     }
 
     /**
