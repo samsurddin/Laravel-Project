40248acae68d1925f3b00675350aa0344537ff4c
Kawsar A <itskawsar@gmail.com>
2022-04-22 17:01:41 +0600

    added db connection

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Multitenancy\Models\Tenant;
 
 class Plan extends Model
 {
     use HasFactory;
+
+    protected $connection = 'tenant';
+
+    public function __construct(array $attributes = array()) {
+        parent::__construct($attributes);
+
+        if (!Tenant::checkCurrent()) {
+            $this->connection = 'landlord';
+        }
+    }
 }
