71422334551e6693100e60ba088eeefceaf12793
Kawsar A <itskawsar@gmail.com>
2022-05-08 04:19:13 +0600

    added head section to add stylesheet or other head tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,9 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
+        {{-- head slot --}}
+        {{ $head ?? '' }}
+
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
         <script>
