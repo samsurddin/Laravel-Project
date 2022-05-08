6dfe993491aefad58eb91acb00425e93965beaa2
Kawsar A <itskawsar@gmail.com>
2022-05-08 21:45:15 +0600

    fixed admin routes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,20 +5,20 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard', app()->getLocale()) }}">
+                    <a href="{{ route('admin.dashboard', app()->getLocale()) }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard', app()->getLocale())" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('admin.dashboard', app()->getLocale())" :active="request()->routeIs('admin.dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('tenants.index', app()->getLocale())" :active="request()->routeIs('tenants.*')">
+                    <x-nav-link :href="route('admin.tenants.index', app()->getLocale())" :active="request()->routeIs('admin.tenants.*')">
                         {{ __('Tenants') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('plans.index', app()->getLocale())" :active="request()->routeIs('plans.*')">
+                    <x-nav-link :href="route('admin.plans.index', app()->getLocale())" :active="request()->routeIs('admin.plans.*')">
                         {{ __('Plans') }}
                     </x-nav-link>
                 </div>
@@ -40,13 +40,13 @@
                     </x-slot>
 
                     <x-slot name="content">
-                        <x-dropdown-link :href="route('users.index', app()->getLocale())" :active="request()->routeIs('users.*')">
+                        <x-dropdown-link :href="route('admin.users.index', app()->getLocale())" :active="request()->routeIs('admin.users.*')">
                             {{ __('User Manager') }}
                         </x-dropdown-link>
-                        <x-dropdown-link :href="route('roles.index', app()->getLocale())" :active="request()->routeIs('roles.*')">
+                        <x-dropdown-link :href="route('admin.roles.index', app()->getLocale())" :active="request()->routeIs('admin.roles.*')">
                             {{ __('Role Manager') }}
                         </x-dropdown-link>
-                        <x-dropdown-link :href="route('settings.index', app()->getLocale())" :active="request()->routeIs('settings.index')">
+                        <x-dropdown-link :href="route('admin.settings.index', app()->getLocale())" :active="request()->routeIs('admin.settings.index')">
                             {{ __('Settings') }}
                         </x-dropdown-link>
 
@@ -79,7 +79,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard', app()->getLocale())" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route('admin.dashboard', app()->getLocale())" :active="request()->routeIs('admin.dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
