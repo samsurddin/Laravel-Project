d3ac8da4a2c2b47fbbb28f96d63fc8e8b1262acf
Kawsar A <itskawsar@gmail.com>
2022-05-08 21:08:51 +0600

    fixed undefine $ issue in ajax setup for CSRF token

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,15 @@ require('./bootstrap');
 // require('./jquery-3.6.0.min');
 global.$ = global.jQuery = require('jquery');
 
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+});
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
